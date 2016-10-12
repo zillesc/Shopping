facf43718d3954c240dba05b8bb2f8042a58c97f
Craig Zilles <zilles@illinois.edu>
2016-10-12 12:43:45 -0500

    Added support for loading UTF8 resource bundles.

--- src/ninja/zilles/Shopping.java
+++ src/ninja/zilles/Shopping.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Shopping {
     private static final int STARTING_NUM_CATS = 7;
     private static int numCats = STARTING_NUM_CATS;
-    private static final ResourceBundle resourceBundle = ResourceBundle.getBundle("ninja.zilles.Resources");
+    private static final ResourceBundle resourceBundle = ResourceBundle.getBundle("ninja.zilles.Resources", new UTF8Control());
     private static final double[] catLimits = {0, 1, 2};
     private static final String[] catStrings = {resourceBundle.getString("nocats"),
             resourceBundle.getString("onecat"), resourceBundle.getString("ncats")};
